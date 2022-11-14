df872a679f022036c71f9fc9c634b9029e809485
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2022-11-14 23:28:01 +0500

    adds choose section and menu slider section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,14 @@
-new Splide( '.splide' , {
+new Splide( '#cta-banner' , {
     type : 'loop' , 
     paging : false ,
     pagination : false 
+} ).mount();
+
+new Splide( '#menu-slider' , {
+    type : 'loop' , 
+    paging : false ,
+    pagination : false ,
+    arrows : false ,
+    perPage : 5 ,
+    gap : "30px"
 } ).mount();
